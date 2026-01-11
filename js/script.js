79184c58bbf83ce9b5925cd05d0f758e535c24cc
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac-2.lan>
2026-01-11 18:49:31 +0100

    Logica sistemata
    Bisogna sistemare lo stile scss

--- js/script.js
+++ js/script.js
@@ -1,19 +1,103 @@
-// Funzione Generica per creare un elemento
-function creaElemento(tag, options = {}) {
-  const elemento = document.createElement(tag);
-  if (options.padre) {
-    options.padre.appendChild(elemento);
+  let listaIdNews = [];
+  let indiceCorrente = 0;
+  const newsPerPagina = 10;
+  const API_BASE = "https://hacker-news.firebaseio.com/v0";
+
+  // Funzione creaElemento
+  function creaElemento(tag, options = {}) {
+    const element = document.createElement(tag);
+    if (options.padre) {
+      options.padre.appendChild(element);
+    }
+    if (options.text !== undefined) {
+      element.textContent = options.text;
+    }
+    if (options.classi) {
+      element.classList.add(...options.classi);
+    }
+    if (options.attributi) {
+      for (const [chiave, valore] of Object.entries(options.attributi)) {
+        element.setAttribute(chiave, valore);
+      }
+    }
+    if (options.eventi) {
+      for (const [event, handler] of Object.entries(options.eventi)) {
+        element.addEventListener(event, handler);
+      }
+    }
+    return element
   }
-  if (options.text !== undefined) {
-    elemento.textContent = options.text;
-  }
-  if (options.classi) {
-    elemento.classList.add(...options.classi);
-  }
-  if (options.attributi) {
-    for (const [chiave, valore] of Object.entries(options.attributi)) {
-      elemento.setAttribute(chiave, valore);
+
+  const contenitoreApp = document.querySelector(".contenitoreApp");
+  const contenitoreNews = creaElemento("div", {
+    classi: ["contenitoreNews"],
+    padre: contenitoreApp,
+  });
+  const titolo = creaElemento("h1", {
+    text: "News in tempo Reale",
+    classi: ["titolo"],
+    padre: contenitoreApp,
+  });
+  const bottoneLoadMore = creaElemento("button", {
+    text: "Carica più News",
+    classi: ["bottoneLoadMore", "btn-primary"],
+    padre: contenitoreApp,
+  });
+
+bottoneLoadMore.addEventListener("click", caricaNewsSuccessive);
+  
+// Funzione Caricamento Lista News
+  
+  async function caricaNewsSuccessive() {
+    const idNewsSuccessive = listaIdNews.slice(
+      indiceCorrente,
+      indiceCorrente + newsPerPagina
+    );
+    for (let id of idNewsSuccessive) {
+      await dettagliNews(id);
+    }
+    indiceCorrente += newsPerPagina;
+    if (indiceCorrente >= listaIdNews.length) {
+      bottoneLoadMore.style.display = "none";
     }
   }
-  return elemento;
-}
+
+  async function listaNews() {
+    try {
+      const response = await fetch(`${API_BASE}/newstories.json`);
+      if (!response.ok) {
+        throw new Error("Fetch Lista news fallito");
+      }
+      listaIdNews = await response.json();
+      console.log(listaIdNews)
+      caricaNewsSuccessive();
+    } catch (error) {
+      console.error("Errore Caricamento News", error);
+    }
+  }
+
+  async function dettagliNews(id) {
+    try {
+      const response = await fetch(`${API_BASE}/item/${id}.json`);
+      if (!response.ok) {
+        throw new Error("Fetch dettagli news Fallito");
+      }
+      const news = await response.json();
+      if (!news) return;
+      console.log(news);
+      const data = new Date(news.time * 1000);
+      const dateString = data.toLocaleString("it-IT");
+      const nuovoItem = creaElemento("div", {
+        classi: ["nuovo-item"],
+        padre: contenitoreNews,
+      });
+      nuovoItem.innerHTML = `<h2><a href="${news.url || "#"}" target="_blank">${
+        news.title
+      }</a></h2>
+        <p class="data">Pubblicato il: ${dateString}</p>`;
+    } catch (error) {}
+  }
+
+  listaNews();
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,54 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+
 module.exports = {
-    entry: "./js/script.js",
-     output: {
+  entry: "./js/script.js",
+  output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    clean: true
-    },
-    plugins: [
-        new Dotenv(),
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-            filename: "index.html"
-        })
-    ],
-module: {
+    clean: true,
+  },
+  mode: "development",
+  devtool: "source-map",
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  module: {
     rules: [
-        {
-            test: /\.scss$/i,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  modules: false,
+                },
+              ],
+            ],
+          },
         },
-        {
-            test: /\.css$/i,
-            use: [
-                "style.loader",
-                "css-loader"
-            ]
-        }
-  ]      
-    },
- devServer: {
+      },
+    ],
+  },
+  devServer: {
     static: "./dist",
     port: 8080,
-    open: true
-  }
-}
+    open: true,
+  },
+};
