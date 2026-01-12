798bbfd757ba0ca4ae1dd6b37f84ea29d21fc30c
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac.fritz.box>
2026-01-12 14:19:14 +0100

    modifiche alla struttura config per far si che gli import scss funzionino
    ora funziona

--- js/script.js
+++ js/script.js
@@ -1,105 +1,103 @@
-import"../scss/style.scss"
+import "../scss/style.scss";
 
 let listaIdNews = [];
-  let indiceCorrente = 0;
-  const newsPerPagina = 10;
-  const API_BASE = "https://hacker-news.firebaseio.com/v0";
+let indiceCorrente = 0;
+const newsPerPagina = 10;
+const API_BASE = "https://hacker-news.firebaseio.com/v0";
 
-  // Funzione creaElemento
-  function creaElemento(tag, options = {}) {
-    const element = document.createElement(tag);
-    if (options.padre) {
-      options.padre.appendChild(element);
-    }
-    if (options.text !== undefined) {
-      element.textContent = options.text;
-    }
-    if (options.classi) {
-      element.classList.add(...options.classi);
-    }
-    if (options.attributi) {
-      for (const [chiave, valore] of Object.entries(options.attributi)) {
-        element.setAttribute(chiave, valore);
-      }
-    }
-    if (options.eventi) {
-      for (const [event, handler] of Object.entries(options.eventi)) {
-        element.addEventListener(event, handler);
-      }
-    }
-    return element
+// Funzione creaElemento
+function creaElemento(tag, options = {}) {
+  const element = document.createElement(tag);
+  if (options.padre) {
+    options.padre.appendChild(element);
   }
+  if (options.text !== undefined) {
+    element.textContent = options.text;
+  }
+  if (options.classi) {
+    element.classList.add(...options.classi);
+  }
+  if (options.attributi) {
+    for (const [chiave, valore] of Object.entries(options.attributi)) {
+      element.setAttribute(chiave, valore);
+    }
+  }
+  if (options.eventi) {
+    for (const [event, handler] of Object.entries(options.eventi)) {
+      element.addEventListener(event, handler);
+    }
+  }
+  return element;
+}
 
-  const contenitoreApp = document.querySelector(".contenitoreApp");
-  const contenitoreNews = creaElemento("div", {
-    classi: ["contenitoreNews"],
-    padre: contenitoreApp,
-  });
-  const titolo = creaElemento("h1", {
-    text: "News in tempo Reale",
-    classi: ["titolo"],
-    padre: contenitoreApp,
-  });
-  const bottoneLoadMore = creaElemento("button", {
-    text: "Carica più News",
-    classi: ["bottoneLoadMore", "btn-primary"],
-    padre: contenitoreApp,
-  });
+const contenitoreApp = document.querySelector(".contenitoreApp");
+const contenitoreNews = creaElemento("div", {
+  classi: ["contenitoreNews"],
+  padre: contenitoreApp,
+});
+const titolo = creaElemento("h1", {
+  text: "News in tempo Reale",
+  classi: ["titolo"],
+  padre: contenitoreApp,
+});
+const bottoneLoadMore = creaElemento("button", {
+  text: "Carica più News",
+  classi: ["bottoneLoadMore", "btn-primary"],
+  padre: contenitoreApp,
+});
 
 bottoneLoadMore.addEventListener("click", caricaNewsSuccessive);
-  
+
 // Funzione Caricamento Lista News
-  
-  async function caricaNewsSuccessive() {
-    const idNewsSuccessive = listaIdNews.slice(
-      indiceCorrente,
-      indiceCorrente + newsPerPagina
-    );
-    for (let id of idNewsSuccessive) {
-      await dettagliNews(id);
-    }
-    indiceCorrente += newsPerPagina;
-    if (indiceCorrente >= listaIdNews.length) {
-      bottoneLoadMore.style.display = "none";
-    }
-  }
 
-  async function listaNews() {
-    try {
-      const response = await fetch(`${API_BASE}/newstories.json`);
-      if (!response.ok) {
-        throw new Error("Fetch Lista news fallito");
-      }
-      listaIdNews = await response.json();
-      console.log(listaIdNews)
-      caricaNewsSuccessive();
-    } catch (error) {
-      console.error("Errore Caricamento News", error);
-    }
+async function caricaNewsSuccessive() {
+  const idNewsSuccessive = listaIdNews.slice(
+    indiceCorrente,
+    indiceCorrente + newsPerPagina
+  );
+  for (let id of idNewsSuccessive) {
+    await dettagliNews(id);
   }
+  indiceCorrente += newsPerPagina;
+  if (indiceCorrente >= listaIdNews.length) {
+    bottoneLoadMore.style.display = "none";
+  }
+}
 
-  async function dettagliNews(id) {
-    try {
-      const response = await fetch(`${API_BASE}/item/${id}.json`);
-      if (!response.ok) {
-        throw new Error("Fetch dettagli news Fallito");
-      }
-      const news = await response.json();
-      if (!news) return;
-      console.log(news);
-      const data = new Date(news.time * 1000);
-      const dateString = data.toLocaleString("it-IT");
-      const nuovoItem = creaElemento("div", {
-        classi: ["nuovo-item"],
-        padre: contenitoreNews,
-      });
-      nuovoItem.innerHTML = `<h2><a href="${news.url || "#"}" target="_blank">${
-        news.title
-      }</a></h2>
+async function listaNews() {
+  try {
+    const response = await fetch(`${API_BASE}/newstories.json`);
+    if (!response.ok) {
+      throw new Error("Fetch Lista news fallito");
+    }
+    listaIdNews = await response.json();
+    console.log(listaIdNews);
+    caricaNewsSuccessive();
+  } catch (error) {
+    console.error("Errore Caricamento News", error);
+  }
+}
+
+async function dettagliNews(id) {
+  try {
+    const response = await fetch(`${API_BASE}/item/${id}.json`);
+    if (!response.ok) {
+      throw new Error("Fetch dettagli news Fallito");
+    }
+    const news = await response.json();
+    if (!news) return;
+    console.log(news);
+    const data = new Date(news.time * 1000);
+    const dateString = data.toLocaleString("it-IT");
+    const nuovoItem = creaElemento("div", {
+      classi: ["nuovo-item"],
+      padre: contenitoreNews,
+    });
+    nuovoItem.innerHTML = `<h2><a href="${news.url || "#"}" target="_blank">${
+      news.title
+    }</a></h2>
         <p class="data">Pubblicato il: ${dateString}</p>`;
-    } catch (error) {}
-  }
+  } catch (error) {}
+}
 
-  listaNews();
-
-
+listaNews();
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,40 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./js/script.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  mode: "development",
-  devtool: "source-map",
-  plugins: [
-    new Dotenv(),
-    new HtmlWebpackPlugin({ template: "./index.html" }),
-    new MiniCssExtractPlugin({ filename: 'style.css' })
-  ],
   module: {
     rules: [
-      // IMPORT SCSS IN JS -> IGNORATO (side-effect: processato da regola sotto)
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-        use: ['ignore-loader? /\.scss$/']  // Ignora solo import SCSS
-      },
-      // PROCESSA SCSS files
       {
         test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
       },
-      // Babel per JS
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false
+        },
       }
     ]
   },
-  devServer: {
-    static: "./dist",
-    port: 8080,
-    open: true,
-  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ],
+  mode: "production"
 };
